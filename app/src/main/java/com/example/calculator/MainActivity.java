bc7f9982c233a248b8171c55a0fed2019a4eb877
Alex <a_v_ilinskiy@mail.ru>
2021-03-21 13:53:46 +0500

    Change background, added button AC (clear TextView)

--- app/src/main/java/com/example/calculator/MainActivity.java
+++ app/src/main/java/com/example/calculator/MainActivity.java
@@ -6,6 +6,8 @@ import android.os.Bundle;
 import android.widget.Button;
 import android.widget.TextView;
 
+import java.util.ArrayList;
+
 public class MainActivity extends AppCompatActivity {
     private Button button_00;
     private Button button_0;
@@ -23,9 +25,13 @@ public class MainActivity extends AppCompatActivity {
     private Button button_multiplication;
     private Button button_divide;
     private Button button_result;
+    private Button button_clear;
 
     private TextView calculationTextView;
     private TextView resultTextView;
+
+    private ArrayList<Integer> counts;
+    private boolean isNextCount;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -36,25 +42,25 @@ public class MainActivity extends AppCompatActivity {
     }
 
     private void initViews() {
-        Button button00 = findViewById(R.id.button_00);
-        Button button0 = findViewById(R.id.button_0);
-        Button button1 = findViewById(R.id.button_1);
-        Button button2 = findViewById(R.id.button_2);
-        Button button3 = findViewById(R.id.button_3);
-        Button button4 = findViewById(R.id.button_4);
-        Button button5 = findViewById(R.id.button_5);
-        Button button6 = findViewById(R.id.button_6);
-        Button button7 = findViewById(R.id.button_7);
-        Button button8 = findViewById(R.id.button_8);
-        Button button9 = findViewById(R.id.button_9);
-        Button button_plus = findViewById(R.id.button_plus);
-        Button button_minus = findViewById(R.id.button_minus);
-        Button button_multiplication = findViewById(R.id.button_multiplication);
-        Button button_divide = findViewById(R.id.button_divide);
-        Button button_result = findViewById(R.id.button_result);
+        button_00 = findViewById(R.id.button_00);
+        button_0 = findViewById(R.id.button_0);
+        button_1 = findViewById(R.id.button_1);
+        button_2 = findViewById(R.id.button_2);
+        button_3 = findViewById(R.id.button_3);
+        button_4 = findViewById(R.id.button_4);
+        button_5 = findViewById(R.id.button_5);
+        button_6 = findViewById(R.id.button_6);
+        button_7 = findViewById(R.id.button_7);
+        button_8 = findViewById(R.id.button_8);
+        button_9 = findViewById(R.id.button_9);
+        button_plus = findViewById(R.id.button_plus);
+        button_minus = findViewById(R.id.button_minus);
+        button_multiplication = findViewById(R.id.button_multiplication);
+        button_divide = findViewById(R.id.button_divide);
+        button_result = findViewById(R.id.button_result);
+        button_clear = findViewById(R.id.button_clear);
 
-        TextView calculationTextView = findViewById(R.id.calculation);
-        TextView resultTextView = findViewById(R.id.result);
-
+        calculationTextView = findViewById(R.id.calculation);
+        resultTextView = findViewById(R.id.result);
     }
 }
