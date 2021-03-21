7382cc52365cd81dac67bc513198560c6da4274e
Alex <a_v_ilinskiy@mail.ru>
2021-03-21 13:14:54 +0500

    Create method initViews

--- app/src/main/java/com/example/calculator/MainActivity.java
+++ app/src/main/java/com/example/calculator/MainActivity.java
@@ -3,12 +3,58 @@ package com.example.calculator;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.widget.Button;
+import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
+    private Button button_00;
+    private Button button_0;
+    private Button button_1;
+    private Button button_2;
+    private Button button_3;
+    private Button button_4;
+    private Button button_5;
+    private Button button_6;
+    private Button button_7;
+    private Button button_8;
+    private Button button_9;
+    private Button button_plus;
+    private Button button_minus;
+    private Button button_multiplication;
+    private Button button_divide;
+    private Button button_result;
+
+    private TextView calculationTextView;
+    private TextView resultTextView;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        initViews();
+    }
+
+    private void initViews() {
+        Button button00 = findViewById(R.id.button_00);
+        Button button0 = findViewById(R.id.button_0);
+        Button button1 = findViewById(R.id.button_1);
+        Button button2 = findViewById(R.id.button_2);
+        Button button3 = findViewById(R.id.button_3);
+        Button button4 = findViewById(R.id.button_4);
+        Button button5 = findViewById(R.id.button_5);
+        Button button6 = findViewById(R.id.button_6);
+        Button button7 = findViewById(R.id.button_7);
+        Button button8 = findViewById(R.id.button_8);
+        Button button9 = findViewById(R.id.button_9);
+        Button button_plus = findViewById(R.id.button_plus);
+        Button button_minus = findViewById(R.id.button_minus);
+        Button button_multiplication = findViewById(R.id.button_multiplication);
+        Button button_divide = findViewById(R.id.button_divide);
+        Button button_result = findViewById(R.id.button_result);
+
+        TextView calculationTextView = findViewById(R.id.calculation);
+        TextView resultTextView = findViewById(R.id.result);
+
     }
 }
