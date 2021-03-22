3a901daae86a09fc51df3d4e8680ec0e91636ca3
Alex <a_v_ilinskiy@mail.ru>
2021-03-22 23:31:12 +0500

    Made method AC (clear all), action button for buttons from 00 to 9, action for buttons plus, minus and result

--- app/src/main/java/com/example/calculator/MainActivity.java
+++ app/src/main/java/com/example/calculator/MainActivity.java
@@ -3,6 +3,7 @@ package com.example.calculator;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.View;
 import android.widget.Button;
 import android.widget.TextView;
 
@@ -28,10 +29,12 @@ public class MainActivity extends AppCompatActivity {
     private Button button_clear;
 
     private TextView calculationTextView;
-    private TextView resultTextView;
+    private TextView bufferTextView;
 
-    private ArrayList<Integer> counts;
-    private boolean isNextCount;
+    private ArrayList<Double> counts = new ArrayList<>();
+    private boolean isNextCount = false;
+    private Double result = 0.0;
+    private String lastOperation;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -61,6 +64,108 @@ public class MainActivity extends AppCompatActivity {
         button_clear = findViewById(R.id.button_clear);
 
         calculationTextView = findViewById(R.id.calculation);
-        resultTextView = findViewById(R.id.result);
+        bufferTextView = findViewById(R.id.bufferCalc);
     }
+
+    public void clearAll(View view) {
+        bufferTextView.setText("");
+        calculationTextView.setText("");
+    }
+
+    public void action_button(View view) {
+        switch (view.getId()) {
+            case R.id.button_00:
+                editText("00");
+                break;
+            case R.id.button_0:
+                editText("0");
+                break;
+            case R.id.button_1:
+                editText("1");
+                break;
+            case R.id.button_2:
+                editText("2");
+                break;
+            case R.id.button_3:
+                editText("3");
+                break;
+            case R.id.button_4:
+                editText("4");
+                break;
+            case R.id.button_5:
+                editText("5");
+                break;
+            case R.id.button_6:
+                editText("6");
+                break;
+            case R.id.button_7:
+                editText("7");
+                break;
+            case R.id.button_8:
+                editText("8");
+                break;
+            case R.id.button_9:
+                editText("9");
+                break;
+            case R.id.button_point:
+                editText(".");
+            default:
+                throw new IllegalStateException("Unexpected value: " + getComponentName().toString());
+        }
+    }
+
+    public void onPlusClick(View view) {
+        Double count = Double.valueOf(calculationTextView.getText().toString());
+        result += count;
+        lastOperation = "+";
+        bufferTextView.setText(calculationTextView.getText().toString() + " + ");
+        calculationTextView.setText("");
+    }
+
+    public void onMinusClick(View view) {
+        Double count = Double.valueOf(calculationTextView.getText().toString());
+        result -= count;
+        lastOperation = "-";
+        bufferTextView.setText(calculationTextView.getText().toString() + " - ");
+        calculationTextView.setText("");
+    }
+
+    /*public void onMiltiClick(View view) {
+        Double count = Double.valueOf(calculationTextView.getText().toString());
+        result *= count;
+        lastOperation = "*";
+        bufferTextView.setText(calculationTextView.getText().toString() + " * ");
+        calculationTextView.setText("");
+    }*/
+
+    private void editText(String count) {
+        String s = String.valueOf(calculationTextView.getText());
+        s += count;
+        calculationTextView.setText(s);
+    }
+
+    public void showResult(View view) {
+        switch (lastOperation) {
+            case "+":
+                result += Double.valueOf(calculationTextView.getText().toString());
+                break;
+            case "-":
+                result -= Double.valueOf(calculationTextView.getText().toString());
+                break;
+            case "*":
+                result *=  Double.valueOf(calculationTextView.getText().toString());
+                break;
+            case "/":
+                result /= Double.valueOf(calculationTextView.getText().toString());
+                break;
+        }
+        calculationTextView.setText(Double.toString(result));
+        isNextCount = false;
+        counts.clear();
+
+        bufferTextView.setText("");
+        result = 0.0;
+    }
+
+
 }
