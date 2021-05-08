36a81ed1ccd232aea05c75f7dea668d1c6cda24c
Alex <a_v_ilinskiy@mail.ru>
2021-05-08 23:45:25 +0500

    Delete init buttons, only findViewById

--- app/src/main/java/com/example/calculator/MainActivity.java
+++ app/src/main/java/com/example/calculator/MainActivity.java
@@ -2,7 +2,6 @@ package com.example.calculator;
 
 import android.os.Bundle;
 import android.view.View;
-import android.widget.Button;
 import android.widget.TextView;
 
 import androidx.appcompat.app.AppCompatActivity;
@@ -10,24 +9,6 @@ import androidx.appcompat.app.AppCompatActivity;
 import java.io.Serializable;
 
 public class MainActivity extends AppCompatActivity implements Serializable {
-    private Button button_00;
-    private Button button_0;
-    private Button button_1;
-    private Button button_2;
-    private Button button_3;
-    private Button button_4;
-    private Button button_5;
-    private Button button_6;
-    private Button button_7;
-    private Button button_8;
-    private Button button_9;
-    private Button button_point;
-    private Button button_plus;
-    private Button button_minus;
-    private Button button_multiplication;
-    private Button button_divide;
-    private Button button_result;
-    private Button button_clear;
 
     private TextView calculationTextView;
     private TextView bufferTextView;
@@ -61,24 +42,23 @@ public class MainActivity extends AppCompatActivity implements Serializable {
     }
 
     private void initViews() {
-        button_0 = findViewById(R.id.button_0);
-        button_1 = findViewById(R.id.button_1);
-        button_2 = findViewById(R.id.button_2);
-        button_3 = findViewById(R.id.button_3);
-        button_4 = findViewById(R.id.button_4);
-        button_5 = findViewById(R.id.button_5);
-        button_6 = findViewById(R.id.button_6);
-        button_7 = findViewById(R.id.button_7);
-        button_8 = findViewById(R.id.button_8);
-        button_9 = findViewById(R.id.button_9);
-        button_point = findViewById(R.id.button_point);
-        button_plus = findViewById(R.id.button_plus);
-        button_minus = findViewById(R.id.button_minus);
-        button_multiplication = findViewById(R.id.button_multiplication);
-        button_divide = findViewById(R.id.button_divide);
-        button_result = findViewById(R.id.button_result);
-        button_clear = findViewById(R.id.button_clear);
-
+        findViewById(R.id.button_0);
+        findViewById(R.id.button_1);
+        findViewById(R.id.button_2);
+        findViewById(R.id.button_3);
+        findViewById(R.id.button_4);
+        findViewById(R.id.button_5);
+        findViewById(R.id.button_6);
+        findViewById(R.id.button_7);
+        findViewById(R.id.button_8);
+        findViewById(R.id.button_9);
+        findViewById(R.id.button_point);
+        findViewById(R.id.button_plus);
+        findViewById(R.id.button_minus);
+        findViewById(R.id.button_multiplication);
+        findViewById(R.id.button_divide);
+        findViewById(R.id.button_result);
+        findViewById(R.id.button_clear);
         calculationTextView = findViewById(R.id.calculation);
         bufferTextView = findViewById(R.id.bufferCalc);
     }
@@ -138,8 +118,7 @@ public class MainActivity extends AppCompatActivity implements Serializable {
     }
 
     public void onMinusClick(View view) {
-        double count = Double.parseDouble(calculationTextView.getText().toString());
-        result = count;
+        result = Double.parseDouble(calculationTextView.getText().toString());
         lastOperation = "-";
         String strResult = calculationTextView.getText().toString();
         bufferTextView.setText(getResources().getString(R.string.result_template, strResult, " - "));
